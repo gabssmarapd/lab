5af6b71eabc76469d6ca08e2438383ee6875eb33
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-10 19:57:20 +0000

    Aula 3

--- projeto-estudo/src/App.js
+++ projeto-estudo/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   const name = 'Gabriel'
+   const newName = name.toUpperCase() 
+
+   const nomecalc = 'Total'
+   const newTotal = nomecalc.toUpperCase()
+
+   const img = 'minha imagem teste'
+   const newImg = img.toUpperCase()
+
+function sum(a, b){
+  return a + b 
+}
   return (
-    <div className="App">
-      <h1>Alterando o JSX</h1>
+    <div className='App'>
+      <h1>TESTE</h1>
       <p>Estudo Projeto</p>
+      <p>Olá, {newName}</p>
+      <p>{newTotal}: {sum(5, 5)}</p>
+      <img src= {URL} alt = {newImg} />
     </div>
   );
 }
