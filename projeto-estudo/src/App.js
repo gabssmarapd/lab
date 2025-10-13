c2c5e03cc4261ea6a0da7c4514d8291a8521f34d
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-13 17:27:58 +0000

    Aula 05

--- projeto-estudo/src/App.js
+++ projeto-estudo/src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import Pessoa from './components/Pessoa';
 
 function App() {
-   const name = 'Gabriel'
-   const newName = name.toUpperCase() 
-
-   const nomecalc = 'Total'
-   const newTotal = nomecalc.toUpperCase()
-
-   const img = 'minha imagem teste'
-   const newImg = img.toUpperCase()
-
-function sum(a, b){
-  return a + b 
-}
   return (
-    <div className='App'>
-      <h1>TESTE</h1>
-      <p>Estudo Projeto</p>
-      <p>Olá, {newName}</p>
-      <p>{newTotal}: {sum(5, 5)}</p>
-      <img src= {URL} alt = {newImg} />
-      <HelloWorld/>
+    <div className="App">
+      <Pessoa nome="Gabriel" 
+              foto="https://placehold.co/100x100?font=roboto"
+              idade="25" 
+              profissao="DEV" />
     </div>
   );
 }
