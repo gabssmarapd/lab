fa102fddb5c262c3dc769eba20e48c7b37680062
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-14 13:28:58 +0000

    TESTE

--- projeto-estudo/src/App.js
+++ projeto-estudo/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List'
+import Evento from './components/Evento';
 
 function App() {
   return (
     <div className="App">
-      <Frase />
       <h1>TESTE</h1>
-      <Pessoa nome="Gabriel" 
-              foto="https://placehold.co/150x150?font=roboto"
-              idade="25" 
-              profissao="DEV" />
-      <List />
     </div>
   );
 }
