5fe4d614eb1b45c97f4a3b39e45821f7e1ea762d
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-14 13:54:21 +0000

    teste

--- projeto-estudo/src/App.js
+++ projeto-estudo/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h1>TESTE</h1>
+      <Evento numero={50} />
+      <Form />
     </div>
   );
 }
