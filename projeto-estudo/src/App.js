e453923ea2fad3e31dfc68c956de6e2e2e3db131
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-15 13:42:18 +0000

    teste

--- projeto-estudo/src/App.js
+++ projeto-estudo/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 function App() {
 
   const meusItens = ['React', 'Vue', 'Angular']
-  
+
   return (
     <div className="App">
-      <h1>Renderização de Listas</h1>
+      <h1>State Lift</h1>
     </div>
   )
 }
