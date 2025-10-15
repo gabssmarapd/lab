41319e067e2f056aba7f0c8e5082cbedb647f790
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-15 18:46:52 +0000

    AULA 12

--- projeto-estudo/src/App.js
+++ projeto-estudo/src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import './App.css';
+import SeuNome from "./components/SeuNome";
+import Saudacao from "./components/Saudacao";
 
 function App() {
-
-  const meusItens = ['React', 'Vue', 'Angular']
+  const [nome, setNome] = useState()
 
   return (
     <div className="App">
       <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome = {nome} />
     </div>
   )
 }
---
+++ projeto-estudo/src/components/Saudacao.js
@@ -0,0 +1,14 @@
+function Saudacao({ nome }) {
+
+    function geraSaudacao(algumNome) {
+        return `Olá, ${algumNome}, tudo bem?`
+    }
+
+    return (
+        <>
+            {nome && <p>{geraSaudacao(nome)}</p>}
+        </>
+    )
+}
+
+export default Saudacao
