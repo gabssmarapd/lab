d66f73b2b5279b3758698ea5aa4971c18c37b176
gabssmarapd <gabrielsmarapd@gmail.com>
2025-10-16 13:37:06 +0000

    Aula 16

--- projeto-estudo/src/components/layout/Footer.js
+++ projeto-estudo/src/components/layout/Footer.js
@@ -1,7 +1,15 @@
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+import Styles from './Footer.module.css'
+
 function Footer() {
   return (
     <footer>
-      <p>Rodapé</p>
+      <ul className={Styles.social_list}>
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaYoutube /></li>
+      </ul>
+      <p>Nosso Rodapé</p>
     </footer>
   )
 }
